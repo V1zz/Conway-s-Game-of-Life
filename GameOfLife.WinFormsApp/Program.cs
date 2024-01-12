44e2836858c86513aea22bd0f350c3b25d3b375a
Ilya <chumachenkoilya@gmail.com>
2024-01-12 14:18:23 +0200

    Add project files.

---
+++ GameOfLife.WinFormsApp/Program.cs
@@ -0,0 +1,17 @@
+// Ignore Spelling: App
+
+namespace GameOfLife.WinFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Ui.MainForm());
+        }
+    }
+}
---
+++ GameOfLife.WinFormsApp/Ui/MainForm.cs
@@ -0,0 +1,10 @@
+namespace GameOfLife.WinFormsApp.Ui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
